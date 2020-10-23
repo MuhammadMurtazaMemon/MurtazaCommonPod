decdc7d2c1518fce52f6341f97bfd8e57550763b
ali qamar <ali.qamar@bsqptech.com>
2020-10-23 18:58:57 +0500

    Initial Commit

---
+++ MurtazaCommonPods/MurtazaCommonPods.h
@@ -0,0 +1,19 @@
+//
+//  MurtazaCommonPods.h
+//  MurtazaCommonPods
+//
+//  Created by Shahzeb siddiqui on 23/10/2020.
+//  Copyright © 2020 Shahzeb siddiqui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MurtazaCommonPods.
+FOUNDATION_EXPORT double MurtazaCommonPodsVersionNumber;
+
+//! Project version string for MurtazaCommonPods.
+FOUNDATION_EXPORT const unsigned char MurtazaCommonPodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MurtazaCommonPods/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MurtazaCommonPods' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MurtazaCommonPods
+
+pod 'IQKeyboardManagerSwift'
+pod 'Alamofire', '4.9.0'
+pod 'SwiftyJSON'
+pod 'Loaf'
+pod 'RealmSwift'
+pod 'SDWebImage'
+pod 'NVActivityIndicatorView'
+
+end
